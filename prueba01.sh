252021b66468035c458c1921021e52d508086092
Cristian Muñoz Rosenfeld <cristian.munoz.rosenfeld@everis.com>
2016-06-09 15:05:49 -0400

    dd

--- prueba01.sh
+++ prueba01.sh
@@ -13,9 +13,6 @@ FECHA_HORA=`date +'%Y%m%d_%H%M%S'`
 VS_NombreLog="JobPilotoCabeceraSinFiltro"
 VS_ARCHIVO_LOG=${PATH_LOG}/${VS_NombreLog}_${FECHA_HORA}.log
 
-echo "Conecta a ssh"
-ssh root@7.212.100.221 -P cloudera
-
 echo "creacion de directorio en hdfs"
 
 #sudo -u hdfs hdfs dfs -mkdir ${PATH_HADOOP}"/prueba11111"
