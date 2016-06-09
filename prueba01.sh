5fc9f0006666d79daa09f20204cf78421d352406
Cristian Muñoz Rosenfeld <cristian.munoz.rosenfeld@everis.com>
2016-06-09 17:15:03 -0400

    pregunta si existe directorio hdfs

--- prueba01.sh
+++ prueba01.sh
@@ -7,6 +7,8 @@ clear
 PATH_HADOOP='/user/cloudera'
 PATH_LOG='/home/cloudera'
 
+$dirpath="/user/cloudera/prueba11111"
+
 # ---------------------------------------------------------------------
 # Parametros: variables
 FECHA_HORA=`date +'%Y%m%d_%H%M%S'`
@@ -16,6 +18,16 @@ VS_ARCHIVO_LOG=${PATH_LOG}/${VS_NombreLog}_${FECHA_HORA}.log
 echo "lista directorios"
 sudo -u hdfs hdfs dfs -ls /user
 
-echo "creacion de directorio en hdfs"
-sudo -u hdfs hdfs dfs -mkdir /user/cloudera/prueba11111
+echo "valido si existe el directorio"
+hadoop fs -test -d $dirpath
+if [ $? != 0 ]
+  then
+    hadoop fs -mkdir $dirpath
+    echo "directorio creado"
+  else
+    echo "Directory already present in HDFS"
+fi
+
+#echo "creacion de directorio en hdfs"
+#sudo -u hdfs hdfs dfs -mkdir /user/cloudera/prueba11111
 
