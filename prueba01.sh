4d004aa0a726dd18b1d3c775db3909d58067b57e
Cristian Muñoz Rosenfeld <cristian.munoz.rosenfeld@everis.com>
2016-06-09 15:50:23 -0400

    sfdfsdfsdf

--- prueba01.sh
+++ prueba01.sh
@@ -13,8 +13,9 @@ FECHA_HORA=`date +'%Y%m%d_%H%M%S'`
 VS_NombreLog="JobPilotoCabeceraSinFiltro"
 VS_ARCHIVO_LOG=${PATH_LOG}/${VS_NombreLog}_${FECHA_HORA}.log
 
+echo "lista directorios"
+sudo -u hdfs hdfs dfs -ls /user
+
 echo "creacion de directorio en hdfs"
 sudo -u hdfs hdfs dfs -mkdir /user/cloudera/prueba11111
 
-echo "lista directorios"
-sudo -u hdfs hdfs dfs -ls /user
