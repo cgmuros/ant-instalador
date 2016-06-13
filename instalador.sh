d8d9b7ed9b078c30dff1ef33a286cccd5c1b4069
Cristian Muñoz Rosenfeld <cristian.munoz.rosenfeld@everis.com>
2016-06-13 10:47:31 -0400

    Se crea nuevo sh limpio para crear estructura en hdfs.

---
+++ instalador.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+
+#Copiar toda la estructura de directorios en hdfs.
+sudo -u hdfs hdfs dfs -put /home/cloudera/instalador/hive /user/cloudera/
+
+
--- prueba01.sh
+++ prueba01.sh
@@ -29,6 +29,4 @@ fi
 #Copiar archivo a hdfs
 sudo -u hdfs hdfs dfs -put /home/cloudera/instalador/hive /user/cloudera/
 
-#echo "creacion de directorio en hdfs"
-#sudo -u hdfs hdfs dfs -mkdir /user/cloudera/prueba11111
 
