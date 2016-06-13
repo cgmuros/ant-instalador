1059974ebdb2dc4caa741b3be9318470a515e116
Cristian Muñoz Rosenfeld <cristian.munoz.rosenfeld@everis.com>
2016-06-13 10:57:27 -0400

    sentencia para eliminar directorios antes de crear.

--- instalador.sh
+++ instalador.sh
@@ -2,6 +2,9 @@
 
 clear
 
+#Elimino de manera recurtsiva todo el directorio hive, archivos y sub carpetas.
+sudo -u hdfs hdfs dfs -rm -R /user/cloudera/hive
+
 #Copiar toda la estructura de directorios en hdfs.
 sudo -u hdfs hdfs dfs -put /home/cloudera/instalador/hive /user/cloudera/
 
